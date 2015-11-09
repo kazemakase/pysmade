3b1a9a4356195dc1400a4f98a35b7a36a7f0a210
Martin Billinger <flkazemakase@gmail.com>
2015-11-09 16:19:03 +0100

    Added Classes for Blueprint meta files

--- devtools/blueprint_files.py
+++ devtools/blueprint_files.py
@@ -28,6 +28,12 @@ class EntityTypes(Enum):
     planet = 4
 
 
+class TagTypes(Enum):
+    finish = 1
+    segment_manager = 2
+    docking = 3
+
+
 class BinFileParser(object):
     def __init__(self, file):
         self.file = file
@@ -42,6 +48,17 @@ class BinFileParser(object):
         if isinstance(decoder, str):
             decoder = struct.Struct(decoder)
         return decoder.unpack(self.file.read(decoder.size))
+
+    def get_one(self, decoder):
+        """
+        Parameters
+        ----------
+        fmt: instance of `struct.Struct`
+             or format string accepted by `struct.unpack`
+        """
+        if isinstance(decoder, str):
+            decoder = struct.Struct(decoder)
+        return decoder.unpack(self.file.read(decoder.size))[0]
 
 
 class Header(object):
@@ -84,3 +101,281 @@ class Header(object):
                "xmax={}, ymax={}, zmax={}, elements={})".format(
             self.version, self.type, self.xmin, self.ymin, self.zmin,
             self.xmax, self.ymax, self.zmax, self.elements)
+
+
+class String(object):
+    @staticmethod
+    def deserialize(stream):
+        len = stream.get_one('>H')
+        return stream.get_one('>%ds' % len).decode('ascii')
+
+    @staticmethod
+    def to_file(file, s):
+        file.write(struct.pack('>H', len(s)))
+        file.write(struct.pack('>%ds' % len(s), s.encode('ascii')))
+
+
+class ByteArray(object):
+    @staticmethod
+    def deserialize(stream):
+        len = stream.get_one('>H')
+        return stream.get_one('>%ds' % len)
+
+    @staticmethod
+    def to_file(file, b):
+        file.write(struct.pack('>H', len(b)))
+        file.write(struct.pack('>%ds' % len(b), b))
+
+
+class TagList(object):
+    def __init__(self, type, tl):
+        self.list = tl
+        self.type = type
+
+    @staticmethod
+    def deserialize(stream):
+        type, length = stream.get('>bI')
+        list = [Payload.deserialize(stream, type) for _ in range(length)]
+        return TagList(type, list)
+
+    def to_file(self, file):
+        file.write(struct.pack('>bI', self.type, len(self.list)))
+        for pl in self.list:
+            pl.to_file(file)
+
+    def __repr__(self):
+        return '[' + ', '.join(repr(pl) for pl in self.list) + ']'
+
+
+class TagStruct(object):
+    def __init__(self, tags):
+        self.tags = tags
+
+    @staticmethod
+    def deserialize(stream):
+        tags = []
+        while True:
+            tag = Tag.deserialize(stream)
+            if tag.type() == 0:
+                break
+            tags.append(tag)
+        return TagStruct(tags)
+
+    def to_file(self, file):
+        for tag in self.tags:
+            tag.to_file(file)
+        EmptyTag.to_file(file)
+
+    def __repr__(self):
+        return '\n'.join(repr(tag) for tag in self.tags)
+
+
+class Tag(object):
+    def __init__(self, name=None, payload=None):
+        self.name = name
+        self.payload = payload
+
+    @staticmethod
+    def deserialize(stream):
+        type = stream.get_one('>b')
+        name = None
+        if type > 0:
+            name = String.deserialize(stream)
+        payload = Payload.deserialize(stream, abs(type))
+        return Tag(name, payload)
+
+    def to_file(self, file):
+        file.write(struct.pack('>b', self.type()))
+        if self.name is not None:
+            String.to_file(file, self.name)
+        self.payload.to_file(file)
+
+    def type(self):
+        if self.payload is None:
+            return 0
+        type = self.payload.type
+        if self.name is None:
+            return -type
+        return type
+
+    def __repr__(self):
+        return "Tag(name={}, payload={})".format(self.name, self.payload)
+
+
+class Payload(object):
+    def __init__(self, type, data):
+        self.type = type
+        self.data = data
+
+    @staticmethod
+    def deserialize(stream, type):
+        if type == 0:
+            data = None
+        elif type == 1:
+            data = stream.get_one('b')  # int8
+        elif type == 2:
+            data = stream.get_one('>h')  # int16
+        elif type == 3:
+            data = stream.get_one('>i')  # int32
+        elif type == 4:
+            data = stream.get_one('>q')  # int64
+        elif type == 5:
+            data = stream.get_one('>f')  # float
+        elif type == 6:
+            data = stream.get_one('>d')  # double
+        elif type == 7:
+            data = ByteArray.deserialize(stream)
+        elif type == 8:
+            data = String.deserialize(stream)
+        elif type == 9:
+            data = stream.get('>fff')  # float vector
+        elif type == 10:
+            data = stream.get('>iii')  # int vector
+        elif type == 11:
+            data = stream.get('>bbb')  # byte vector
+        elif type == 12:
+            data = TagList.deserialize(stream)
+        elif type == 13:
+            data = TagStruct.deserialize(stream)
+        elif type == 14:
+            data = stream.get_one('b')  # factory registration
+        elif type == 15:
+            data = stream.get('>ffff')  # float 4 vector
+        else:
+            raise ValueError('unknown tag-payload type: {}'.format(type))
+        return Payload(type, data)
+
+    def to_file(self, file):
+        if self.type == 0:
+            return
+        elif self.type == 1:
+            file.write(struct.pack('b', self.data))
+        elif self.type == 2:
+            file.write(struct.pack('>h', self.data))
+        elif self.type == 3:
+            file.write(struct.pack('>i', self.data))
+        elif self.type == 4:
+            file.write(struct.pack('>q', self.data))
+        elif self.type == 5:
+            file.write(struct.pack('>f', self.data))
+        elif self.type == 6:
+            file.write(struct.pack('>d', self.data))
+        elif self.type == 7:
+            ByteArray.to_file(file, self.data)
+        elif self.type == 8:
+            String.to_file(file, self.data)
+        elif self.type == 9:
+            file.write(struct.pack('>fff', *self.data))
+        elif self.type == 10:
+            file.write(struct.pack('>iii', *self.data))
+        elif self.type == 11:
+            file.write(struct.pack('>bbb', *self.data))
+        elif self.type == 12:
+            self.data.to_file(file)
+        elif self.type == 13:
+            self.data.to_file(file)
+        elif self.type == 14:
+            file.write(struct.pack('b', self.data))
+        elif self.type == 15:
+            file.write(struct.pack('>ffff', *self.data))
+        else:
+            raise ValueError('unknown tag-payload type: {}'.format(self.type))
+
+    def __repr__(self):
+        return repr(self.data)
+
+
+EmptyPayload = Payload(0, None)
+EmptyTag = Tag(name=None, payload=EmptyPayload)
+
+
+class TagRoot(object):
+    def __init__(self, version=0, tag=EmptyTag):
+        self.version = version
+        self.tag = tag
+
+    @staticmethod
+    def deserialize(stream):
+        version = stream.get_one('>H')
+        if version == 0x1:
+            raise NotImplementedError('Compressed tags not supported yet.')
+        tag = Tag.deserialize(stream)
+        return TagRoot(version, tag)
+
+    def to_file(self, file):
+        file.write(struct.pack('>H', self.version))
+        self.tag.to_file(file)
+
+    def __repr__(self):
+        return "TagRoot(version={}, tag={}".format(self.version, self.tag)
+
+
+class MetaDockedEntry(object):
+    def __init__(self, name, pos, size, style, orientation):
+        self.name = name
+        self.pos = tuple(pos)
+        self.size = tuple(size)
+        self.style = style
+        self.orientation = orientation
+
+    @staticmethod
+    def from_file(stream):
+        name = String.deserialize(stream)
+        pos = stream.get('>iii')
+        size = stream.get('>fff')
+        style, orientation = stream.get('>hb')
+        return MetaDockedEntry(name, pos, size, style, orientation)
+
+    def to_file(self, file):
+        String.to_file(file, self.name)
+        file.write(struct.pack('>iii', *self.pos))
+        file.write(struct.pack('>fff', *self.size))
+        file.write(struct.pack('>hb', self.style, self.orientation))
+
+
+class Meta(object):
+    def __init__(self, version, docked=None, tags=None):
+        self.version = version
+        self.docked = docked
+        self.tags = tags
+
+    @staticmethod
+    def from_file(filename):
+        with open(filename, 'rb') as file:
+            stream = BinFileParser(file)
+            version, = stream.get('>i')
+            docked = None
+            tags = None
+            while True:
+                tag = TagTypes(stream.get_one('>b'))
+
+                if tag == TagTypes.finish:
+                    break
+                elif tag == TagTypes.segment_manager:
+                    tags = TagRoot.deserialize(stream)
+                    break
+                elif tag == TagTypes.docking:
+                    docked_count = stream.get_one('>I')
+                    docked = [MetaDockedEntry.from_file(stream) for _ in
+                              range(docked_count)]
+
+        return Meta(version, docked, tags)
+
+    def to_file(self, filename):
+        with open(filename, 'wb') as file:
+            file.write(struct.pack('>i', self.version))
+
+            if self.docked is not None:
+                file.write(struct.pack('>b', TagTypes.docking.value))
+                file.write(struct.pack('>I', len(self.docked)))
+                for d in self.docked:
+                    d.to_file(file)
+
+            if self.tags is not None:
+                file.write(struct.pack('>b', TagTypes.segment_manager.value))
+                self.tags.to_file(file)
+
+            file.write(struct.pack('>b', TagTypes.finish.value))
+
+#meta = Meta.from_file("../data/Isanth-VI/meta.smbpm")
+#meta = Meta.from_file("/home/billinger/.local/share/Steam/SteamApps/common/StarMade/StarMade/blueprints-stations/pirate/Piratestation Alpha/meta.smbpm")
--- tests/test_blueprint_files.py
+++ tests/test_blueprint_files.py
@@ -21,10 +21,13 @@
 """
 
 import os
+import struct
 
 from nose.tools import assert_equal, assert_tuple_equal
 
 from devtools.blueprint_files import BinFileParser, EntityTypes, Header
+from devtools.blueprint_files import Meta, MetaDockedEntry, TagRoot, Tag
+from devtools.blueprint_files import Payload, TagStruct, TagList
 
 
 def assert_headers_equal(a, b):
@@ -37,6 +40,53 @@ def assert_headers_equal(a, b):
     assert_equal(a.ymax, b.ymax)
     assert_equal(a.zmax, b.zmax)
     assert_equal(a.elements, b.elements)
+
+
+def assert_meta_equal(a, b):
+    assert_equal(a.version, b.version)
+    for da, db in zip(a.docked, b.docked):
+        assert_docked_equal(da, db)
+    assert_equal(a.tags.version, b.tags.version)
+    assert_tags_equal(a.tags.tag, b.tags.tag)
+
+
+def assert_docked_equal(a, b):
+    assert_equal(a.name, b.name)
+    assert_equal(a.pos, b.pos)
+    assert_equal(a.size, b.size)
+    assert_equal(a.style, b.style)
+    assert_equal(a.orientation, b.orientation)
+
+
+def assert_tags_equal(a, b):
+    assert_equal(a.name, b.name)
+    assert_payload_equal(a.payload, b.payload)
+
+
+def assert_taglist_equal(a, b):
+    assert_equal(a.type, b.type)
+    assert_equal(len(a.list), len(b.list))
+    for pa, pb in zip(a.list, b.list):
+        assert_payload_equal(pa, pb)
+
+
+def assert_tagstruct_equal(a, b):
+    assert_equal(len(a.tags), len(b.tags))
+    for ta, tb in zip(a.tags, b.tags):
+        assert_tags_equal(ta, tb)
+
+
+def assert_payload_equal(a, b):
+    assert_equal(a.type, b.type)
+    if a.type == 0:
+        assert_equal(a.data, None)
+        assert_equal(b.data, None)
+    elif a.type in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 14, 15]:
+        assert_equal(a.data, b.data)
+    elif a.type == 12:
+        assert_taglist_equal(a.data, b.data)
+    elif a.type == 13:
+        assert_tagstruct_equal(a.data, b.data)
 
 
 def test_binfileparser():
@@ -59,3 +109,24 @@ def test_header_read_write_consistency():
     copied = Header.from_file('test.smbph')
     os.remove('test.smbph')
     assert_headers_equal(original, copied)
+
+
+def test_meta():
+    docked = [MetaDockedEntry("name", [1, 2, 3], [1.0, 2.0, 3.0], 7, -1)]
+
+    bytearray_tag = Tag(None, Payload(7, b'123'))
+
+    list_tag = Tag(None, Payload(12, TagList(1, [Payload(1, 1), Payload(1, 2), Payload(1, 3)])))
+
+    named_vector_tag = Tag("named_vector", Payload(10, (0, 4, 2)))
+
+    struct_tag = Tag(None, Payload(13, TagStruct([list_tag, bytearray_tag, named_vector_tag])))
+
+    tags = TagRoot(version=0, tag=struct_tag)
+
+    meta = Meta(version=0, docked=docked, tags=tags)
+    meta.to_file('test.smbpm')
+
+    reload = Meta.from_file('test.smbpm')
+
+    assert_meta_equal(meta, reload)
